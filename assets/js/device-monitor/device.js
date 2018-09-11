c5825e1fd93e546d878a6b471b339637e24dfc0b
Subash Pathak <subash@subash.me>
2018-09-11 09:57:41 +0545

    Cleanup

--- assets/js/device-monitor/device.js
+++ assets/js/device-monitor/device.js
@@ -6,10 +6,8 @@ export default function Device(props) {
       <div className="DeviceMonitor__Device__icon">
         <i className="icon-network"></i>
       </div>
-      <div className="DeviceMonitor__Device__info">
-        <div className="DeviceMonitor__Device__name">{props.device.deviceName}</div>
-        <div className="DeviceMonitor__Device__ip">{props.device.ip}</div>
-      </div>
+      <div className="DeviceMonitor__Device__name">{props.device.deviceName}</div>
+      <div className="DeviceMonitor__Device__ip">{props.device.ip}</div>
     </a>
   );
 }

a3629a57b8e31a5b911c85c5a2c4c2c042a17362
Subash Pathak <subash@subash.me>
2018-12-10 17:58:44 +0545

    Update devices.js

--- src/devices.js
+++ src/devices.js
@@ -1,7 +1,7 @@
 const devices = [
-  { deviceName: 'Router', ip: '192.168.1.1' },    
-  { deviceName: 'MCN Modem', ip: '10.0.0.10' },
-  { deviceName: 'Techminds Station', ip: '172.18.70.201' },
+  { deviceName: 'Router', ip: '192.168.1.1' },
+  { deviceName: 'MCN Modem', ip: '10.0.0.20' },
+  { deviceName: 'Techminds Modem', ip: '10.0.0.10' },
   { deviceName: 'Terrace AP', ip: '192.168.1.8' },
   { deviceName: 'Subash\'s Room AP', ip: '192.168.1.5' },
   { deviceName: 'Living Room AP', ip: '192.168.1.6' },

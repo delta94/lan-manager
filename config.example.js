a57ac98e24577eca6727c69fd09589348accaf62
Subash Pathak <subash@subash.me>
2019-02-23 15:47:32 +0545

    Unifi

--- config.example.js
+++ config.example.js
@@ -3,5 +3,11 @@ module.exports = {
     address: '192.168.1.1',
     username: 'admin',
     password: 'password'
+  },
+  unifi: {
+    address: 'https://unifi.example.com',
+    site: 'site-id',
+    username: 'admin@example.com',
+    password: 'password'
   }
 }
---
+++ src/utils/unifi.js
@@ -0,0 +1,36 @@
+const rp = require('request-promise-native');
+const config = require('../../config.js');
+
+const request = rp.defaults({
+  jar: true,
+  simple: false,
+  json: true,
+  baseUrl: config.unifi.address,
+});
+
+async function isLoggedIn() {
+  const res = await request('/api/self/sites');
+  return res.meta.rc === 'ok';
+}
+
+async function login() {
+  const { username, password } = config.unifi;
+  const res = await request.post('/api/login', { body: { username, password }});
+  if(res.meta.rc === 'ok') return;
+  throw new Error('Login Failed');
+}
+
+async function loginIfRequired() {
+  if(!(await isLoggedIn())) await login();
+}
+
+async function requestSite(path, { options }) {
+  await loginIfRequired();
+  const res = await request(`/api/s/${config.unifi.site}${path}`, options);
+  if(res.meta.rc !== 'ok') throw new Error(res.meta.msg);
+  return res.data;
+}
+
+module.exports = {
+  request: requestSite
+}

6d0a686b8e8c960988f72f11f5ba7e66b5a6985c
Subash Pathak <subash@subash.me>
2019-02-27 20:29:08 +0545

    Update app.js

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import DeviceMonitor from './device-monitor/device-monitor';
 import Speed from './speed'

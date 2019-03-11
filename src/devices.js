ec691ece1ded4dd652ec28b2da6fd8d4a5c36cae
Subash Pathak <subash@subash.me>
2019-03-11 16:53:12 +0545

    Update devices.js

--- src/devices.js
+++ src/devices.js
@@ -4,7 +4,7 @@ module.exports = [
   { deviceName: `Techminds ONU`, ip: `10.0.0.30` },
   { deviceName: `Subash's Room AP`, ip: `192.168.1.5` },
   { deviceName: `Living Room AP`, ip: `192.168.1.6` },
-  { deviceName: `Terrace AP`, ip: `192.168.1.7` },
-  { deviceName: `Nishan's Station`, ip: `192.168.2.3` },
-  { deviceName: `Nishan's Router`, ip: `192.168.2.4` }
+  { deviceName: `Nishan's AP`, ip: `192.168.2.3` },
+  { deviceName: `Nishan's Station`, ip: `192.168.2.4` },
+  { deviceName: `Nishan's Router`, ip: `192.168.2.5` }
 ];

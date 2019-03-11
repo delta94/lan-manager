3dd13fef5212ddd10ddada9a46a109f5cba7e837
Subash Pathak <subash@subash.me>
2019-03-11 23:53:24 +0545

    Update devices.js

--- src/devices.js
+++ src/devices.js
@@ -5,6 +5,6 @@ module.exports = [
   { deviceName: `Subash's Room AP`, ip: `192.168.1.5` },
   { deviceName: `Living Room AP`, ip: `192.168.1.6` },
   { deviceName: `Nishan's AP`, ip: `192.168.2.3` },
-  { deviceName: `Nishan's Station`, ip: `192.168.2.4` },
+  { deviceName: `Nishan's STA`, ip: `192.168.2.4` },
   { deviceName: `Nishan's Router`, ip: `192.168.2.5` }
 ];

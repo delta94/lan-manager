b6a84430684eb74e170ff45a669599491dcdc8e6
Subash Pathak <subash@subashpathak.com>
2019-05-08 20:37:23 +0545

    Update device-monitor.js

--- app/src/device-monitor/device-monitor.js
+++ app/src/device-monitor/device-monitor.js
@@ -1,58 +1,31 @@
 import React, { Component } from 'react';
 import Device from './device';
+import useApi from '../hooks/use-polling-api';
 
-export default class DeviceMonitor extends Component {
+export default function DeviceMonitor() {
+  const { error, data: devices, loading } = useApi(`/api/devices`);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      devices: []
-    };
-  }
-
-  componentWillMount() {
-    this.loadDevices();
-  }
-
-  componentDidMount() {
-    this.startDeviceMonitor();
-  }
-
-  componentWillUnmount() {
-    this.stopDeviceMonitor();
-  }
-
-  startDeviceMonitor() {
-    this.deviceTimer = setInterval(async ()=> {
-      this.loadDevices();
-    }, 3000);
-  }
-
-  stopDeviceMonitor() {
-    clearInterval(this.deviceTimer);
-  }
-
-  async loadDevices() {
-    const response = await fetch("/api/devices");
-    const json = await response.json();
-    if(json.error)  {
-      let error = new Error(json.message);
-      error.data = json.data;
-      error.SERVER_ERROR = true;
-      console.error(error);
-    }
-
-    this.state.devices = json.data;
-    this.setState(this.state);
-  }
-
-  render() {
+  if(error) {
     return (
       <div className="DeviceMonitor">
-        {this.state.devices.map(device => {
-          return <Device device={device} key={device.ip}/>
-        })}
+        { error.message }
       </div>
     );
   }
+
+  if(loading) {
+    return (
+      <div className="DeviceMonitor">
+       Loading...
+      </div>
+    );
+  }
+
+  return (
+    <div className="DeviceMonitor">
+    {devices.map(device => {
+      return <Device device={device} key={device.ip}/>
+    })}
+    </div>
+  );
 }

63da78d1b85ee3c2785be10ccd74007bf7770223
Subash Pathak <subash@subashpathak.com>
2019-06-02 13:46:05 +0545

    Power Monitor

--- src/utils/is-reachable.js
+++ src/utils/is-reachable.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 module.exports = function isReachable(host) {
   return new Promise((resolve, reject) => {
     //Internal function no sanitation required
-    exec(`ping -c 1 "${host}"`, { timeout: 100 }, (error) => {
+    exec(`ping -c 1 "${host}"`, { timeout: 300 }, (error) => {
       resolve(!error);
     });
   });

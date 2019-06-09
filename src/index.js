0b327217f35ea363b0b426b844c6882e4bb29904
Subash Pathak <subash@subashpathak.com>
2019-06-09 21:59:04 +0545

    powermonitor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const api = require('./api');
-const tasks = require('./tasks');
 const config = require('../config.js');
 const middlewares = require('./utils/middlewares');
 const app = express();
@@ -12,7 +11,5 @@ app.use(middlewares);
 app.use('/api', api);
 app.use(express.static(path.resolve(__dirname, '../static')));
 app.listen(config.port);
-
-tasks.init();
 
 console.log(`Listening on port ${config.port}`);
--- src/tasks.js
+++
@@ -1,5 +0,0 @@
-const { logStatus } = require('./utils/power-monitor');
-
-exports.init = function init() {
-  setInterval(logStatus, 60 * 1000); //Monitor electricity every minute
-}

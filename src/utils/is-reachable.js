01ad36f0a415f532cb3a374032c8f7c586e4ad24
Subash Pathak <subash@subashpathak.com>
2019-07-24 17:33:45 +0545

    Update is-reachable.js

--- src/utils/is-reachable.js
+++ src/utils/is-reachable.js
@@ -3,8 +3,6 @@ const { exec } = require('child_process');
 module.exports = function isReachable(host) {
   return new Promise((resolve, reject) => {
     //Internal function no sanitation required
-    exec(`ping -c 1 "${host}"`, { timeout: 300 }, (error) => {
-      resolve(!error);
-    });
+    exec(`ping -c 1 "${host}"`, { timeout: 300 }, err=> resolve(!err));
   });
 }

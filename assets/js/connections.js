a851e53e3bbaf039d678c5906bd67dc641cc8b62
Subash Pathak <subash@subashpathak.com>
2019-07-24 22:05:04 +0545

    Update connections.js

--- assets/js/connections.js
+++ assets/js/connections.js
@@ -1,15 +1,113 @@
-import React from 'react';
+import React, { useState, useReducer, useEffect } from 'react';
+
+const initialState = {
+  loading: true,
+  error: null,
+  connections: []
+};
+
+async function load() {
+  const response = await fetch("/api/connections");
+  const json = await response.json();
+  if(!json.error) return json.data;
+  let err = new Error(json.message);
+  err.data = json.data;
+  throw err;
+}
+
+async function refresh(connectionName) {
+  const response = await fetch(`/api/connections/refresh/${connectionName}`, { method: 'post' });
+  const json = await response.json();
+  if(!json.error) return;
+  let err = new Error(json.message);
+  err.data = json.data;
+  throw err;
+}
+
+async function prefer(connectionName) {
+  const response = await fetch(`/api/connections/prefer/${connectionName}`, { method: 'post' });
+  const json = await response.json();
+  if(!json.error) return;
+  let err = new Error(json.message);
+  err.data = json.data;
+  throw err;
+}
+
+function swallowError(promise) {
+  promise.catch((err)=> console.error(err));
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case 'LOADING':
+      return { ...state, loading: true };
+    case 'ERROR':
+      return { ...state, error: action.error };
+    case 'LOAD':
+      return { loading: false, error: null, connections: action.connections };
+    case 'PREFER':
+      state.connections.map(c=> c.preferred = false); // Remove preferred from other connections
+      action.connection.preferred = true;
+      return { ...state, connections };
+    default:
+      throw new Error(`Invalid action ${action.type}`);
+  }
+}
 
 export default function Connections() {
+  const [ state, dispatch ] = useReducer(reducer, initialState);
+  const [ pollData, setPollData ] = useState(true);
+
+  // Load on mount
+  useEffect(()=> {
+    load()
+      .then(connections=> dispatch({ type: 'LOAD', connections }))
+      .catch((err)=> {
+        dispatch({ type: 'LOADING', loading: false });
+        dispatch({ type: 'ERROR', error: err });
+      });
+  }, []);
+
+  // Reload data every 3 seconds
+  useEffect(()=> {
+    if(!pollData) return;
+    const interval = setInterval(()=> swallowError(
+      load().then(connections=> dispatch({ type: 'LOAD', connections }))
+    ), 3 * 1000);
+    return ()=> clearInterval(interval);
+  }, [pollData]);
+
+  if(state.error || state.loading) {
+    return (
+      <div className="Connections">...</div>
+    );
+  }
+
   return (
     <div className="Connections">
-      <Connection label="ABCD" running={true} disabled={false} active={true} preferred={true} />
-      <Connection label="ABCD" running={true} disabled={false} active={false} preferred={false} />
+      {state.connections.map(connection => (
+        <Connection
+          key={connection.connectionName}
+          label={connection.label}
+          running={connection.running}
+          disabled={connection.disabled}
+          active={connection.active}
+          preferred={connection.preferred}
+          onRefresh={()=> swallowError(refresh(connection.connectionName))}
+          onPrefer={()=> {
+            setPollData(false); // Give server time to prefer a connection
+            dispatch({ type: 'PREFER', connection: connection });
+            swallowError(
+              prefer(connection.connectionName).finally(()=> setPollData(true)) // Restart polling
+            );
+          }}
+        />
+      ))}
     </div>
   );
 }
 
-function Connection({ label, running, disabled, active, preferred }) {
+function Connection({ label, running, disabled, active, preferred, onRefresh, onPrefer }) {
   const className = [
     `Connection`,
     `Connection--${running ? 'running': 'not-running'}`,
@@ -18,19 +116,24 @@ function Connection({ label, running, disabled, active, preferred }) {
     `Connection--${preferred ? 'preferred': 'not-preferred'}`
   ].join(' ');
 
-  const renderPreferredIcon = ()=> (
-    <div className="Connection__preferred-icon" title="Preferred Connection">
-      <i className="icon-favorite"></i>
-    </div>
+  const refreshButton = ()=> (
+    <button className="Connection__action" title="Refresh Connection" onClick={onRefresh}>
+      <i className="icon-refresh"></i> Refresh
+    </button>
+  );
+
+  const preferButton = ()=> (
+    <button className="Connection__action" title="Refresh Connection" onClick={onPrefer}>
+      <i className="icon-favorite"></i> Prefer
+    </button>
   );
 
   return (
     <a className={className}>
-      { preferred ? renderPreferredIcon() : null }
       <div className="Connection__label">{label}</div>
       <div className="Connection__actions">
-        <button className="Connection__action" title="Refresh Connection"><i className="icon-refresh"></i> Refresh</button>
-        <button className="Connection__action" title="Prefer Connection"><i className="icon-refresh"></i> Prefer</button>
+        { refreshButton() }
+        { !preferred ? preferButton() : null }
       </div>
     </a>
   );

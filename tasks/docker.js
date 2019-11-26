84b55a838e55c5ac0471fac75dc45e3b6e4d61c0
Subash Pathak <subash@subashpathak.com>
2019-11-26 12:09:57 +0545

    Update docker.js

--- tasks/docker.js
+++ tasks/docker.js
@@ -5,7 +5,7 @@ const ROOT_DIR = path.resolve(__dirname, '../');
 
 async function getTag() {
   const out = await run('git rev-parse --short HEAD', { cwd: ROOT_DIR });
-  return `registry.gitlab.com/sbspk/containers/lan-manager:${out.trim()}`;
+  return `subash/lan-manager:${out.trim()}`;
 }
 
 async function build() {
